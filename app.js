a291fda95b3a70ff1f650ea14e8cad4f297368ed
Raksha Pawar <rakshapawar10.sae@gmail.com>
2021-07-26 13:43:33 +0530

    feat: Translating the english text to banana text

--- app.js
+++ app.js
@@ -3,10 +3,26 @@ let btnTranslate = document.querySelector("#btn-translate");
 let txtInput = document.querySelector("#txt-input");
 let outputDiv = document.querySelector("#output");
 
+const serverURL = "https://api.funtranslations.com/translate/minion.json";
+// Fetch call
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+// Error Handler
+function errorHandler(error) {
+  console.log("Error Occured " + error);
+}
+
 // Event Handlers
 function clickHandler() {
-  let input = txtInput.value;
-  outputDiv.innerText = "Translated " + input;
+  let inputText = txtInput.value;
+
+  //   Calling server for processing.
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => (outputDiv.innerHTML = json.contents.translated))
+    .catch(errorHandler);
 }
 
 // Event Listeners
